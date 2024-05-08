8d6dfac802fba5ced667b40ef7f93125c7532af4
Angel G. Ortiz <ortizangelg23@gmail.com>
2024-05-08 14:54:15 -0400

    started project

---
+++ src/app/games/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+function games() {
+  return (
+    <div>games</div>
+  )
+}
+
+export default games
---
+++ src/app/handsigns/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HandSigns() {
+  return (
+    <div>HandSigns</div>
+  )
+}
+
+export default HandSigns
